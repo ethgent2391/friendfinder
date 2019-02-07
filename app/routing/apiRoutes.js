48f210ab78325b4c0d6ab57d51840e2e7cc7b3b3
Ethan Gent <ethan.gent.2391@gmail.com>
2019-02-07 17:55:46 -0500

    trying to fix the hroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,34 +10,34 @@ module.exports = function(app) {
         res.json(friendsData);
     });
 
-    function findBestFriend(user) {
+    function findBFF(user) {
         var userScores = user.scores;
 
 
 
-        var maxDiff = 4 * 10;
-        var winningFriend;
+        var max = 4 * 10;
+        var bestie;
 
         for (var i = 0; i < friendsData.length; i++) {
             var friendScores = friendsData[i].scores;
-            var totalDiff = 0;
+            var total = 0;
 
             for (var j = 0; j < friendScores.length; j++) {
-                totalDiff = totalDiff + Math.abs(userScores[j] - friendScores[j]);
+                total = total + Math.abs(userScores[j] - friendScores[j]);
             }
-            if (totalDiff <= maxDiff) {
-                winningFriend = friendsData[i];
-                maxDiff = totalDiff;
+            if (total <= max) {
+                bestie = friendsData[i];
+                max = total;
             }
         }
-        return winningFriend;
+        return bestie;
     }
 
 
     app.post("/api/friends", function(req, res) {
         var currentUser = req.body;
-        var bestFriend = findBestFriend(currentUser);
-        res.json(bestFriend);
+        var BFF = findBFF(currentUser);
+        res.json(BFF);
         friendsData.push(currentUser);
     });
     app.post("/api/clear", function() {
